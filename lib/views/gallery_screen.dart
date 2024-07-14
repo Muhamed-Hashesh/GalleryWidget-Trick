e324bb9495a01ec676537b4f9124f0986e0486db
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-07-15 01:16:26 +0300

    gallery.

--- lib/views/gallery_screen.dart
+++ lib/views/gallery_screen.dart
@@ -1,6 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gallery_app/items_list.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({super.key});
